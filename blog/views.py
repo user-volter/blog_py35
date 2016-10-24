43926705ec1a9bd78f9656954a0b3e374c123d88
volter_k <vl99@inmae.com>
2016-10-25 00:43:27 +0300

    My Django app, first commit

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,13 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})	 
 
 def post_new(request):
     if request.method == "POST":
@@ -22,8 +19,12 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(instance=post)
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -37,4 +38,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})  
+    return render(request, 'blog/post_edit.html', {'form': form})
